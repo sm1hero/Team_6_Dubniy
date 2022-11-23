edb9d5b743630897149fac5075d04014907d5aab
XTeam_6_1 <smirnovandryushnka01@gmail.com>
2022-11-23 11:24:33 +0300

    первый бот

--- gleb.py
+++ gleb.py
@@ -1,7 +1,7 @@
 from TOKEN import token
 import telebot
 bot = telebot.TeleBot(token)
-@bot.message_handler(commands=['start'])ав
+@bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
 bot.polling()
