28d25003035897f4fa00da6a6a003a7a7a521943
XTeam_6_1 <smirnovandryushnka01@gmail.com>
2022-11-23 11:55:17 +0300

    первый бот

--- gleb.py
+++ gleb.py
@@ -3,12 +3,15 @@ import telebot
 bot = telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+    if message.chat.id == 1658431458:
+        bot.send_message(message.chat.id, 'Здравствуйте! Желаете добавить, редактировать или удалить занятие?')
 bot.polling()
 class Raspisanie():
-    def __init__(self, day, group, time):
-        """Constructor"""
+    def __init__(self, day, type, group, time, teacher, number):
         self.day = day
+        self.type = type
         self.group = group
         self.time = time
+        self.teacher = teacher
+        self.number = number
 
