38296ddbc6df25657b3d394fa71b08125bf4c499
XTeam_6_1 <smirnovandryushnka01@gmail.com>
2022-11-23 15:39:37 +0300

    финал

--- gleb.py
+++ gleb.py
@@ -2,73 +2,55 @@ from TOKEN import token
 import telebot
 from telebot import types
 
+#основной словарь
+raspisanie = {}
+course = ""
+
 bot = telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton("День недели")
-    item2 = types.KeyboardButton("Фамилии учителей")
-    item3 = types.KeyboardButton("Номера кабинетов")
-    markup.add(item1, item2, item3)
-    item4 = types.KeyboardButton("Добавить")
-    item5 = types.KeyboardButton("Редактировать")
-    item6 = types.KeyboardButton("Удалить")
-    markup1.add(item4, item5, item6)
+    # определяем админа по id
     if message.chat.id == 1658431458:
-        bot.send_message(message.chat.id, 'Здравствуйте! Желаете добавить, редактировать или удалить занятие?'.format(message.from_user,
-                                                                                                     bot.get_me()),
-                         reply_markup=markup1)
+        bot.send_message(message.chat.id, 'Здравствуйте! Напишите "добавить" чтобы начать')
     else:
-        bot.send_message(message.chat.id, 'Привет! Напиши мне день/учителя/номер кабинета и я покажу тебе расписание!'.format(message.from_user,
-                                                                                                     bot.get_me()),
-                         reply_markup=markup)
-@bot.message_handler(content_types=['text'])
-def chating(message):
-    if message.chat.type == 'private':
-        if message.text == 'Расписание занятий':
-            markup = types.InlineKeyboardMarkup(row_width=2)
-            item1 = types.InlineKeyboardButton('День недели', callback_data='dayofweek')
-            item2 = types.InlineKeyboardButton('Фамилия педагога', callback_data='surname')
-            item3 = types.InlineKeyboardButton('Номер кабинета', callback_data='number')
-            markup.add(item1, item2, item3)
-            bot.send_message(message.chat.id, 'Выберите, как вы хотите реализовать поиск :', reply_markup=markup)
+        bot.send_message(message.chat.id, 'Привет! Напиши мне день и я покажу тебе расписание!')
+@bot.message_handler(content_types='text')
+def message_reg(message):
+    if message.chat.id == 1658431458:
+        if message.text.lower() == 'добавить':
+            bot.send_message(message.chat.id, "Опишите курс (время и название): ")
+    else:
+        lessons = []
+        # если по этому дню информации нет получается ошибка
+        try:
+            while True:
+                lessons.append(raspisanie.pop(message))
+        except:
+            if lessons == lessons.clear():
+                bot.send_message(message.chat.id, "Я не понимаю вас!")
+                start_message(message)
+            else:
+                a = ""
+                for i in range(0, len(lessons)):
+                    a += lessons[i] + ", "
+                bot.send_message(message.chat.id, "Занятия: " + a)
 
-                if message.text == 'День недели':
-                    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-                    item1 = types.KeyboardButton('Понедельник')
-                    item2 = types.KeyboardButton('Вторник')
-                    item3 = types.KeyboardButton('Среда')
-                    item4 = types.KeyboardButton('Четверг')
-                    item5 = types.KeyboardButton('Пятница')
-                    item6 = types.KeyboardButton('Суббота')
-                    item7 = types.KeyboardButton('Воскресенье')
-                    markup.add(item1, item2, item3, item4, item5, item6, item7)
-                    bot.send_message(message.chat.id, 'Выберите день недели', reply_markup=markup)
+@bot.message_handler(content_types='text')
+def message_admin(message):
+    if message.chat.id == 1658431458:
+        course = message.text
+        bot.send_message(message.chat.id, "Напишите день недели, кабинет и фамилию учителя (через запятую): ")
+    else:
+        start_message(message)
 
-                    @bot.callback_query_handler(func=lambda call: True)
-                    def callback_inline(call):
-                        if call.message:
-                            if call.data == 'dayofweek':
-                                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.chat_id,
-                                                      text='Введите день недели: ')
-                            elif call.data == 'surname':
-                                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.chat_id,
-                                                      text='Введите фамилию педагога: ')
-                            elif call.data == 'number':
-                                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.chat_id,
-                                                      text='Введите номер кабинета: ')
-
-                                if message.text == 'День недели':
-                                    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-                                    item1 = types.KeyboardButton('Понедельник')
-                                    item2 = types.KeyboardButton('Вторник')
-                                    item3 = types.KeyboardButton('Среда')
-                                    item4 = types.KeyboardButton('Четверг')
-                                    item5 = types.KeyboardButton('Пятница')
-                                    item6 = types.KeyboardButton('Суббота')
-                                    item7 = types.KeyboardButton('Воскресенье')
-                                    markup.add(item1, item2, item3, item4, item5, item6, item7)
-                                    bot.send_message(message.chat.id, 'Выберите день недели', reply_markup=markup)
-
+@bot.message_handler(content_types='text')
+def message_admin(message):
+    if message.chat.id == 1658431458:
+        params = message.text.split(",")
+        raspisanie[params[0]] = course
+        raspisanie[params[1]] = course
+        raspisanie[params[2]] = course
+    else:
+        start_message(message)
 
 bot.polling()
--- main.py
+++ main.py
@@ -1,16 +1,55 @@
-# This is a sample Python script.
+import telebot
+from telebot import types
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#основной словарь
+raspisanie = {}
+course = ""
 
+bot = telebot.TeleBot(5980407272:AAG5q_k07BI4G_hP8jX4KtkQUS30DVNee4g)
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    # определяем админа по id
+    if message.chat.id == 1658431458:
+        bot.send_message(message.chat.id, 'Здравствуйте! Напишите "добавить" чтобы начать')
+    else:
+        bot.send_message(message.chat.id, 'Привет! Напиши мне день и я покажу тебе расписание!')
+@bot.message_handler(content_types='text')
+def message_reg(message):
+    if message.chat.id == 1658431458:
+        if message.text.lower() == 'добавить':
+            bot.send_message(message.chat.id, "Опишите курс (время и название): ")
+    else:
+        lessons = []
+        # если по этому дню информации нет получается ошибка
+        try:
+            while True:
+                lessons.append(raspisanie.pop(message))
+        except:
+            if lessons == lessons.clear():
+                bot.send_message(message.chat.id, "Я не понимаю вас!")
+                start_message(message)
+            else:
+                a = ""
+                for i in range(0, len(lessons)):
+                    a += lessons[i] + ", "
+                bot.send_message(message.chat.id, "Занятия: " + a)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@bot.message_handler(content_types='text')
+def message_admin(message):
+    if message.chat.id == 1658431458:
+        course = message.text
+        bot.send_message(message.chat.id, "Напишите день недели, кабинет и фамилию учителя (через запятую): ")
+    else:
+        start_message(message)
 
+@bot.message_handler(content_types='text')
+def message_admin(message):
+    if message.chat.id == 1658431458:
+        params = message.text.split(",")
+        raspisanie[params[0]] = course
+        raspisanie[params[1]] = course
+        raspisanie[params[2]] = course
+    else:
+        start_message(message)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+bot.polling()
