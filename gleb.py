97ee400804a78f25d487ec15ca178ef486b6dc36
XTeam_6_1 <smirnovandryushnka01@gmail.com>
2022-11-23 11:04:11 +0300

    первый ботик

---
+++ gleb.py
@@ -0,0 +1,7 @@
+import telebot
+from TOKEN import token
+bot=telebot.Telebot(token)
+@bot.message_handler(commands=['start'])
+def start_message(message):
+  bot.send_message(message.chat.id,"Привет ✌️ ")
+bot.infinity_poling()
